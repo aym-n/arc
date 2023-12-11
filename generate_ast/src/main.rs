a458a618daf88616056fa7cc685db76d13bfcc59
aym-n <ajm8982@gmail.com>
2023-12-11 19:05:01 +0530

    added constructors to genrate_ast

--- generate_ast/src/main.rs
+++ generate_ast/src/main.rs
@@ -7,10 +7,10 @@ fn main(){
             let args: Vec<String> = std::env::args().collect();
             let output_dir = &args[1];
             define_ast(output_dir, "Expr", &[
-                "Binary : Expr left, Token operator, Expr right",
-                "Grouping : Expr expression",
-                "Literal : Object value",
-                "Unary : Token operator, Expr right",
+                "Binary : Box<Expr> left, Token operator, Box<Expr> right",
+                "Grouping : Box<Expr> expression",
+                "Literal : i64 value",
+                "Unary : Token operator, Box<Expr> right",
             ]);
         },
         _ => {
@@ -27,33 +27,65 @@ fn define_ast(output_dir: &str, base_name: &str, types: &[&str]) {
     file.write_all(b"use crate::tokens::Token;\n").unwrap();
     file.write_all(b"use crate::tokens::TokenKind;\n").unwrap();
 
-    file.write_all(format!("pub enum {} {{\n", base_name).as_bytes()).unwrap();
-
     for type_ in types {
         let type_name = type_.split(":").next().unwrap().trim();
         let fields = type_.split(":").nth(1).unwrap().trim();
         define_type(&mut file, type_name, fields);
+        define_constructor(&mut file, type_name, fields);
     }
-
-    file.write_all(b"}\n").unwrap();
-
 }
 
 fn define_type(file: &mut fs::File, type_name: &str, fields: &str) {
-    file.write_all(format!("    {} {{\n", type_name).as_bytes()).unwrap();
 
+    file.write_all(format!("pub struct {} {{\n", type_name).as_bytes()).unwrap();
     let fields: Vec<&str> = fields.split(",").collect();
     for field in fields {
+
         let field = field.trim();
         let field_name = field.split(" ").nth(1).unwrap().trim();
-        let mut field_type = field.split(" ").nth(0).unwrap().trim();
-        if field_type == "Expr" {
-            field_type = "Box<Expr>";
+        let field_type = field.split(" ").nth(0).unwrap().trim();
+
+        file.write_all(format!("    pub {}: {},\n", field_name, field_type).as_bytes()).unwrap();
+    }
+    file.write_all(b"}\n").unwrap();
+}
+
+fn define_constructor(file: &mut fs::File, type_name: &str, fields: &str) {
+    // impl LiteralExpr {
+    //     pub fn new(value: i64) -> Self {
+    //         Self {
+    //             value,
+    //         }
+    //     }
+    // }
+    file.write_all(format!("impl {} {{\n", type_name).as_bytes()).unwrap();
+    file.write_all(format!("    pub fn new(").as_bytes()).unwrap();
+    let fields: Vec<&str> = fields.split(",").collect();
+    let mut first = true;
+    let parameters = fields.clone();
+    for field in fields {
+
+        let field = field.trim();
+        let field_name = field.split(" ").nth(1).unwrap().trim();
+        let field_type = field.split(" ").nth(0).unwrap().trim();
+        if first {
+            first = false;
+        } else {
+            file.write_all(format!(", ").as_bytes()).unwrap();
         }
-        file.write_all(format!("        {}: {},\n", field_name, field_type).as_bytes()).unwrap();
+        file.write_all(format!("{}: {} ", field_name, field_type).as_bytes()).unwrap();
+    }
+    file.write_all(format!(") -> Self {{\n").as_bytes()).unwrap();
+    
+    for parameter in parameters {
+        let parameter = parameter.trim();
+        let parameter_name = parameter.split(" ").nth(1).unwrap().trim();
+        file.write_all(format!("        {},\n", parameter_name).as_bytes()).unwrap();
     }
 
+    file.write_all(format!("    }}\n").as_bytes()).unwrap();
 
-    file.write_all(b"    },\n").unwrap();
+    file.write_all(format!("}}\n").as_bytes()).unwrap();
 }
+
 
--- src/expr.rs
+++ src/expr.rs
@@ -1,19 +1,40 @@
 use crate::tokens::Token;
 use crate::tokens::TokenKind;
-pub enum Expr {
-    Binary {
-        left: Box<Expr>,
-        operator: Token,
-        right: Box<Expr>,
-    },
-    Grouping {
-        expression: Box<Expr>,
-    },
-    Literal {
-        value: Object,
-    },
-    Unary {
-        operator: Token,
-        right: Box<Expr>,
-    },
+pub struct Binary {
+    pub left: Box<Expr>,
+    pub operator: Token,
+    pub right: Box<Expr>,
+}
+impl Binary {
+    pub fn new(left: Box<Expr> , operator: Token , right: Box<Expr> ) -> Self {
+        left,
+        operator,
+        right,
+    }
+}
+pub struct Grouping {
+    pub expression: Box<Expr>,
+}
+impl Grouping {
+    pub fn new(expression: Box<Expr> ) -> Self {
+        expression,
+    }
+}
+pub struct Literal {
+    pub value: i64,
+}
+impl Literal {
+    pub fn new(value: i64 ) -> Self {
+        value,
+    }
+}
+pub struct Unary {
+    pub operator: Token,
+    pub right: Box<Expr>,
+}
+impl Unary {
+    pub fn new(operator: Token , right: Box<Expr> ) -> Self {
+        operator,
+        right,
+    }
 }
